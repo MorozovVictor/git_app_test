2cc3c72589574f3e7034da85decbe6ea60f3a7ac
Victor Morozov <morozovvic@gmail.com>
2017-10-31 22:54:40 +0200

    Add a cycle

--- src/com/java/git/test/Main.java
+++ src/com/java/git/test/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git");
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+
+        }
     }
 }
